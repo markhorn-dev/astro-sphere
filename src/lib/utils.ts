9423b0343c0867a86693e57f593900b7bbb5a43c
datle-dev <dle.dev@outlook.com>
2024-08-26 11:05:35 -0400

    Add utility function for retrieving album images

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,3 +19,19 @@ export function readingTime(html: string) {
   const readingTimeMinutes = ((wordCount / 200) + 1).toFixed()
   return `${readingTimeMinutes} min read`
 }
+
+export async function getAlbumImages(albumId: string) {
+  let images = import.meta.glob<{ default: ImageMetadata }>(
+    "/src/content/albums/**/*.{jpeg,jpg,png}"
+  );
+
+  images = Object.fromEntries(
+    Object.entries(images).filter(([key]) => key.includes(albumId))
+  );
+
+  const resolvedImages = await Promise.all(
+    Object.values(images).map((image) => image().then((mod) => mod.default))
+  );
+
+  return resolvedImages;
+}
