978ca30e61c668f0dab8be394cfa58b172ae2c7c
luasenvy <luas.envy@gmail.com>
2024-10-23 16:55:33 +0900

    fix: career motion

--- components/CareerLayout.tsx
+++ components/CareerLayout.tsx
@@ -17,14 +17,7 @@ export default function CareerLayout() {
   return (
     <div className="flex-1 py-5">
       <Container size="md">
-        <motion.ul
-          variants={{
-            hidden: { opacity: 0, y: 20 },
-            block: { opacity: 1, y: 0, transition: { staggerChildren: 0.16 } },
-          }}
-          initial="hidden"
-          animate="block"
-        >
+        <ul>
           {careers.map(({ Component, metadata }, i) => (
             <motion.li
               key={`career-${i}`}
@@ -32,6 +25,9 @@ export default function CareerLayout() {
                 hidden: { opacity: 0, y: 20 },
                 block: { opacity: 1, y: 0, transition: { duration: 0.56 } },
               }}
+              initial="hidden"
+              whileInView="block"
+              viewport={{ once: true, amount: 0.4 }}
               className="border-b border-black/10 dark:border-white/25 mt-4 py-8 first-of-type:mt-0 first-of-type:pt-0 last-of-type:border-none"
             >
               <div className="text-sm uppercase mb-4">
@@ -42,7 +38,7 @@ export default function CareerLayout() {
               <article className="prose dark:prose-invert">{Component && <Component />}</article>
             </motion.li>
           ))}
-        </motion.ul>
+        </ul>
       </Container>
     </div>
   );
