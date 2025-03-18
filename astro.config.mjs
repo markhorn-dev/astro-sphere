e6bae69ec067ccad36e1e219d5afd441b1272928
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-03-18 20:40:55 -0300

    Updates CNAME and Astro settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-sphere-demo.vercel.app",
+  site: "https://ggteixeira.github.io",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
