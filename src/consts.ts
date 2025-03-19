ed5f596fed8d9e0c2ee69ffde1d04b277e397bf9
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-03-18 23:08:41 -0300

    Adds personalized info

--- src/consts.ts
+++ src/consts.ts
@@ -1,81 +1,80 @@
-import type { Site, Page, Links, Socials } from "@types"
+import type { Site, Page, Links, Socials } from "@types";
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
-}
+  TITLE: "Guilherme Teixeira",
+  DESCRIPTION: "Welcome my website.",
+  AUTHOR: "Guilherme Teixeira",
+};
 
 // Work Page
 export const WORK: Page = {
   TITLE: "Work",
   DESCRIPTION: "Places I have worked.",
-}
+};
 
 // Blog Page
 export const BLOG: Page = {
   TITLE: "Blog",
   DESCRIPTION: "Writing on topics I am passionate about.",
-}
+};
 
-// Projects Page 
+// Projects Page
 export const PROJECTS: Page = {
   TITLE: "Projects",
   DESCRIPTION: "Recent projects I have worked on.",
-}
+};
 
 // Search Page
 export const SEARCH: Page = {
   TITLE: "Search",
   DESCRIPTION: "Search all posts and projects by keyword.",
-}
+};
 
 // Links
 export const LINKS: Links = [
-  { 
-    TEXT: "Home", 
-    HREF: "/", 
+  {
+    TEXT: "Home",
+    HREF: "/",
   },
-  { 
-    TEXT: "Work", 
-    HREF: "/work", 
+  {
+    TEXT: "Work",
+    HREF: "/work",
   },
-  { 
-    TEXT: "Blog", 
-    HREF: "/blog", 
+  {
+    TEXT: "Blog",
+    HREF: "/blog",
   },
-  { 
-    TEXT: "Projects", 
-    HREF: "/projects", 
+  {
+    TEXT: "Projects",
+    HREF: "/projects",
   },
-]
+];
 
 // Socials
 export const SOCIALS: Socials = [
-  { 
+  {
     NAME: "Email",
-    ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    ICON: "email",
+    TEXT: "hello@guiteixeira.dev",
+    HREF: "mailto:hello@guiteixeira.dev",
   },
-  { 
+  {
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "ggteixeira",
+    HREF: "https://github.com/ggteixeira",
   },
-  { 
+  {
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "guiatf",
+    HREF: "https://www.linkedin.com/in/guiatf/",
   },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+  {
+    NAME: "Bluesky",
+    ICON: "bluesky",
+    TEXT: "guiteixeira.dev",
+    HREF: "https://bsky.app/profile/guiteixeira.dev",
   },
-]
-
+];
