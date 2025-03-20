b1d2023717703e542ee80a3215d81c85d73ce77d
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-03-19 22:43:04 -0300

    Updates About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,12 @@
+import { currentYear } from "@lib/utils";
+
 export default function About() {
   return (
-      <section class="animate">
-        <article>
-          <p>I am a <b><i>software engineer</i></b>, <b><i>ui/ux designer</i></b>, <b><i>product planner</i></b>, <b><i>mentor</i></b>, <b><i>student</i></b>, <b><i>minimalist</i></b>, <b><i>eternal optimist</i></b>, <b><i>crypto enthusiast</i></b> and <b><i>sarcasm connoisseur</i></b>.</p>
-          <p>I love to both build and break things. I am motivated by challenging projects with self-guided research and dynamic problem solving. My true passion is crafting creative front end designs with unique takes on color, typography and motion.</p>
-          <p>During my career</p>
-          <p>I have built products ranging from marketing and ecommerce websites to complex enterprise apps with focus on delivering fast, elegant code along with delightful user interfaces.</p>
-          <p>Now</p>
-          <p>I currently work as a software engineer at StreamlineFS, where I do product planning, design and development.</p>
-        </article>
-      </section>
-
-
+    <section class="animate">
+      <article>
+        <p>I am a <b><i>software engineer</i></b> with {currentYear - 2018} years of experience in the technology industry, {currentYear - 2020} of which as a <b><i>front-end developer</i></b>.</p>
+        <p>Besides <b><i>front-end development</i></b>, I am also interested in <b><i>back-end development</i></b>, <b><i>functional programming</i></b>, <b><i>open-source</i></b>, and <b><i>security and privacy</i></b>.</p>
+      </article>
+    </section>
   )
 }
