939ddfa813f37e8f25399496d642e0f7167163e4
Tanner Sims <tannersims@hesitantlyhuman.com>
2025-04-19 16:57:07 -0600

    Styling fixes are completed, rendering changes partially done

--- public/js/copy.js
+++ public/js/copy.js
@@ -1,29 +1,36 @@
 const codeBlocks = document.querySelectorAll('pre:has(code)');
 
-//add copy btn to every code block on the dom
-codeBlocks.forEach((code) => {
-  //button icon
-  const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-  use.setAttribute('href', '/copy.svg#empty');
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.classList.add('copy-svg');
-  svg.appendChild(use);
+// //add copy btn to every code block on the dom
+// codeBlocks.forEach((code) => {
+//   // button icons
+//   const use_empty = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+//   use_empty.setAttribute('href', '/copy.svg#empty');
+//   const copy_svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+//   copy_svg.classList.add('copy-svg', 'copy-svg-inactive');
+//   copy_svg.appendChild(use_empty);
 
-  //create button
-  const btn = document.createElement('button');
-  btn.appendChild(svg);
-  btn.classList.add('copy-btn');
-  btn.addEventListener('click', (e) => copyCode(e));
+//   const use_filled = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+//   use_filled.setAttribute('href', '/copy.svg#filled');
+//   const copied_svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+//   copied_svg.classList.add('copy-svg', 'copy-svg-active');
+//   copied_svg.appendChild(use_filled);
 
-  //container to fix copy button
-  const container = document.createElement('div');
-  container.classList.add('copy-cnt');
-  container.appendChild(btn);
+//   // create button
+//   const btn = document.createElement('button');
+//   btn.appendChild(copied_svg);
+//   btn.appendChild(copy_svg);
+//   btn.classList.add('copy-btn');
+//   btn.addEventListener('mousedown', (e) => copyCode(e));
 
-  //add to code block
-  code.classList.add('relative');
-  code.appendChild(container);
-});
+//   // container to fix copy button
+//   const container = document.createElement('div');
+//   container.classList.add('copy-cnt');
+//   container.appendChild(btn);
+
+//   // add to code block
+//   code.classList.add('relative');
+//   code.insertBefore(container, code.getElementsByTagName("code")[0]);
+// });
 
 /**
 * @param {MouseEvent} event
@@ -31,13 +38,6 @@ codeBlocks.forEach((code) => {
 function copyCode(event) {
   let codeBlock = getChildByTagName(event.currentTarget.parentElement.parentElement, 'CODE')
   navigator.clipboard.writeText(codeBlock.innerText)
-  const use = getChildByTagName(getChildByTagName(event.currentTarget, 'svg'), 'use');
-  use.setAttribute('href', '/copy.svg#filled')
-  setTimeout(() => {
-    if (use) {
-      use.setAttribute('href', '/copy.svg#empty')
-    }
-  }, 100);
 }
 
 function getChildByTagName(element, tagName) {
