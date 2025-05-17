a03ab65021fba01792b2bd1b23b4470558c80370
Tanner Sims <tannersims@hesitantlyhuman.com>
2025-05-17 08:50:57 -0600

    All improvements to code snippet implemented

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 
+import { transformerMetaHighlight } from '@shikijs/transformers';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
@@ -16,8 +18,9 @@ export default defineConfig({
           // Somewhere along the line, the properties are getting encoded in a
           // way which does not preserve whitespace. See CodeSnippet.astro
           hast.properties['data-code'] = JSON.stringify(this.source);
-        }
-      }],
+        },
+      },
+    transformerMetaHighlight()],
       themes: {
         light: "github-light",
         dark: "dark-plus",
