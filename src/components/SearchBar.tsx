78094b72c39cd1638df53ed699a2e9c93b7d5a8b
Tanner Sims <tannerjeffreysims@gmail.com>
2025-05-18 03:46:25 -0600

    Fix search bar overflow issue

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -10,7 +10,7 @@ export default function SearchBar({ onSearchInput, query, setQuery, placeholderT
         <svg class="absolute size-6 left-2 top-[0.45rem] stroke-neutral-400 dark:stroke-neutral-500 pointer-events-none">
             <use href={`/ui.svg#search`} />
         </svg>
-        <input name="search" type="text" value={query()} onInput={onSearchInput} autocomplete="off" spellcheck={false} placeholder={placeholderText} class="w-full px-2.5 py-1.5 pl-10 rounded outline-none placeholder-neutral-400 dark:placeholder-neutral-500 text-black dark:text-white bg-black/5 dark:bg-white/10 hover:bg-black/10 hover:dark:bg-white/15 focus:bg-black/10 focus:dark:bg-white/15 border border-black/10 dark:border-white/10 focus:border-black/40 focus:dark:border-white/40" />
+        <input name="search" type="text" value={query()} onInput={onSearchInput} autocomplete="off" spellcheck={false} placeholder={placeholderText} class="w-full px-10 py-1.5 rounded outline-none placeholder-neutral-400 dark:placeholder-neutral-500 text-black dark:text-white bg-black/5 dark:bg-white/10 hover:bg-black/10 hover:dark:bg-white/15 focus:bg-black/10 focus:dark:bg-white/15 border border-black/10 dark:border-white/10 focus:border-black/40 focus:dark:border-white/40" />
         {query().length > 0 && (
             <button
                 onClick={() => setQuery("")}
