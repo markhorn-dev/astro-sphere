f7e72366587929fa5f22a6ccd2d3960aed177bf0
Tanner Sims <tannerjeffreysims@gmail.com>
2025-05-17 20:20:37 -0600

    Add search bar to Blog.tsx

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,5 +1,6 @@
 import type { CollectionEntry } from "astro:content"
 import { createEffect, createSignal, For } from "solid-js"
+import Fuse from "fuse.js"
 import ArrowCard from "@components/ArrowCard"
 import { cn } from "@lib/utils"
 
@@ -9,13 +10,21 @@ type Props = {
 }
 
 export default function Blog({ data, tags }: Props) {
+  const [query, setQuery] = createSignal("");
   const [filter, setFilter] = createSignal(new Set<string>())
   const [posts, setPosts] = createSignal<CollectionEntry<"blog">[]>([])
 
+  const fuse = new Fuse(data, {
+    keys: ["slug", "data.title", "data.summary", "data.tags"],
+    includeMatches: true,
+    minMatchCharLength: 2,
+    threshold: 0.4,
+  })
+
   createEffect(() => {
-    setPosts(data.filter((entry) => 
-      Array.from(filter()).every((value) => 
-        entry.data.tags.some((tag:string) => 
+    setPosts((query().length < 2 ? data : fuse.search(query()).map((result) => result.item)).filter((entry) =>
+      Array.from(filter()).every((value) =>
+        entry.data.tags.some((tag: string) =>
           tag.toLowerCase() === String(value).toLowerCase()
         )
       )
@@ -23,36 +32,87 @@ export default function Blog({ data, tags }: Props) {
   })
 
   function toggleTag(tag: string) {
-    setFilter((prev) => 
-      new Set(prev.has(tag) 
-        ? [...prev].filter((t) => t !== tag) 
+    setFilter((prev) =>
+      new Set(prev.has(tag)
+        ? [...prev].filter((t) => t !== tag)
         : [...prev, tag]
       )
     )
   }
 
+  const onSearchInput = (e: Event) => {
+    const target = e.target as HTMLInputElement
+    setQuery(target.value)
+  }
+
   return (
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
+      {/* Control Panel*/}
       <div class="col-span-3 sm:col-span-1">
         <div class="sticky top-24">
-          <div class="text-sm font-semibold uppercase mb-2 text-black dark:text-white">Filter</div>
+          {/* Search Bar */}
+          <div class="relative">
+            <svg class="absolute size-6 left-2 top-[0.45rem] stroke-neutral-400 dark:stroke-neutral-500 pointer-events-none">
+              <use href={`/ui.svg#search`} />
+            </svg>
+            <input name="search" type="text" value={query()} onInput={onSearchInput} autocomplete="off" spellcheck={false} placeholder="Search posts" class="w-full px-2.5 py-1.5 pl-10 rounded outline-none text-black dark:text-white bg-black/5 dark:bg-white/10 hover:bg-black/10 hover:dark:bg-white/15 focus:bg-black/10 focus:dark:bg-white/15 border border-black/10 dark:border-white/10 focus:border-black/40 focus:dark:border-white/40" />
+            {query().length > 0 && (
+              <button
+                onClick={() => setQuery("")}
+                class="absolute flex justify-center items-center h-full w-10 right-0 top-0 stroke-neutral-400 dark:stroke-neutral-500 hover:stroke-neutral-600 hover:dark:stroke-neutral-300"
+              >
+                <svg class="size-5">
+                  <use href={`/ui.svg#x`} />
+                </svg>
+              </button>
+            )}
+          </div>
+          {/* Tag Filters */}
+          <div class="text-sm font-semibold uppercase my-4 text-black dark:text-white">Tags</div>
           <ul class="flex flex-wrap sm:flex-col gap-1.5">
             <For each={tags}>
               {(tag) => (
-                <li>
-                  <button onClick={() => toggleTag(tag)} class={cn("w-full px-2 py-1 rounded", "whitespace-nowrap overflow-hidden overflow-ellipsis", "flex gap-2 items-center", "bg-black/5 dark:bg-white/10", "hover:bg-black/10 hover:dark:bg-white/15", "transition-colors duration-300 ease-in-out", filter().has(tag) && "text-black dark:text-white")}>
-                    <svg class={cn("size-5 fill-black/50 dark:fill-white/50", "transition-colors duration-300 ease-in-out", filter().has(tag) && "fill-black dark:fill-white")}>
-                      <use href={`/ui.svg#square`} class={cn(!filter().has(tag) ? "block" : "hidden")} />
-                      <use href={`/ui.svg#square-check`} class={cn(filter().has(tag) ? "block" : "hidden")} />
+                <li class="md:w-full">
+                  <button
+                    onClick={() => toggleTag(tag)}
+                    class={cn(
+                      "w-full px-2 py-1 rounded",
+                      "flex gap-2 items-center",
+                      "bg-black/5 dark:bg-white/10",
+                      "hover:bg-black/10 hover:dark:bg-white/15",
+                      "transition-colors duration-300 ease-in-out",
+                      filter().has(tag) && "text-black dark:text-white"
+                    )}
+                  >
+                    <svg
+                      class={cn(
+                        "shrink-0 size-5 fill-black/50 dark:fill-white/50",
+                        "transition-colors duration-300 ease-in-out",
+                        filter().has(tag) && "fill-black dark:fill-white"
+                      )}
+                    >
+                      <use
+                        href={`/ui.svg#square`}
+                        class={cn(!filter().has(tag) ? "block" : "hidden")}
+                      />
+                      <use
+                        href={`/ui.svg#square-check`}
+                        class={cn(filter().has(tag) ? "block" : "hidden")}
+                      />
                     </svg>
-                    {tag}
+
+                    <span class="truncate block min-w-0">
+                      {tag}
+                    </span>
                   </button>
+
                 </li>
               )}
             </For>
           </ul>
         </div>
       </div>
+      {/* Posts */}
       <div class="col-span-3 sm:col-span-2">
         <div class="flex flex-col">
           <div class="text-sm uppercase mb-2">
